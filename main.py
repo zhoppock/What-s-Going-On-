8af6d0e39ee49db93146705dce7a967f384297d7
Zachary Hoppock <61926087+zhoppock@users.noreply.github.com>
2022-06-10 19:10:57 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,34 @@
+from time import sleep
+from replit import audio
+import os
+
+source = audio.play_file("whatsup.wav")
+
+while True:
+  pass
+  sleep(30.25)
+  print("25 years and my life is still") ; sleep(2.25) ; os.system('clear')
+  print("Tryin' to get up that great big hill of hope") ; sleep(6) ; os.system('clear')
+  print("For a destination") ; sleep(5) ; os.system('clear')
+  print("I realized quickly when I knew I should") ; sleep(2.4) ; os.system('clear')
+  print("That the world was made up of this brotherhood of man") ; sleep(6) ; os.system('clear')
+  print("For whatever that means") ; sleep(2.4) ; os.system('clear')
+  sleep(100)
+  print("And I try") ; sleep(2.4) ; os.system('clear')
+  print("Oh my god do I try") ; sleep(3) ; os.system('clear')
+  print("I try all the time") ; sleep(3.7) ; os.system('clear')
+  print("In this institution") ; sleep(4.2) ; os.system('clear')
+  print("And I pray") ; sleep(2.5) ; os.system('clear')
+  print("Oh my god do I pray") ; sleep(3) ; os.system('clear')
+  print("I pray every single day!") ; sleep(3.7) ; os.system('clear')
+  print("for revolution!") ; sleep(4) ; os.system('clear')
+  print("And so I cry sometimes when I'm lying in bed") ; sleep(3.5) ; os.system('clear')
+  print("just to get it all out, what's in my head and I'm") ; sleep(4) ; os.system('clear')
+  print("Feeling") ; sleep(2) ; os.system('clear')
+  print("A little peculiar") ; sleep(3.5) ; os.system('clear')
+  print("And so I wake up in the morning and I step outside") ; sleep(3) ; os.system('clear')
+  print("just to take a deep breathe and get real high") ; sleep(3) ; os.system('clear')
+  print("And I") ; sleep(1) ; os.system('clear')
+  print("scream at the top of my lungs") ; sleep(1.5) ; os.system('clear')
+  print("What's going on!") ; sleep(2.5) ; os.system('clear')
+  print("And I say") ; sleep(0.3) ; os.system('clear')
